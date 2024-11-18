c89717121eacffaa1eafc3f4277384239afb4b9f
victor <103121630+victor-914@users.noreply.github.com>
2024-11-18 13:06:38 +0100

    status

--- pages/properties/index.js
+++ pages/properties/index.js
@@ -128,3 +128,19 @@ export const getStaticProps = async () => {
     return {};
   }
 };
+
+export async function getStaticPaths() {
+  const res = await api.get(
+    `/houses?&pagination[page]=1&pagination[pageSize]=5`
+  );
+  const paths = res?.data?.data?.map((item) => ({
+    params: { houseId: item.id.toString() },
+  }));
+  return { paths, fallback: true };
+}
+
+export async function getStaticProps({ params }) {
+  let data = await api.get(`/houses/${params.houseId.toString()}?populate=*`);
+  data = data?.data;
+  return { props: { data }, revalidate: 60 };
+}
